1f360c1be6009d89e546fbbdff0551b588395911
Brant Watrous <watroubd@gmail.com>
2013-01-21 02:04:13 -0800

    Allow engines to prepend to additional_import_paths

--- lib/iridium/compass.rb
+++ lib/iridium/compass.rb
@@ -16,7 +16,9 @@ module Iridium
       app.config.compass.project_path = app.root
       app.config.compass.sprite_load_path = app.all_paths[:sprites].expanded
       app.config.compass.generated_images_path = app.site_path.join('images').to_s
-      app.config.compass.additional_import_paths = [app.vendor_path.join("stylesheets")]
+      # Sass files aren't being loaded properly from engines. One workaround is to
+      # add the engine's app/stylesheets path to additional_import_paths
+      (app.config.compass.additional_import_paths ||= []) << app.vendor_path.join("stylesheets")
 
       Compass.add_configuration app.config.compass
     end
